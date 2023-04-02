1c7460b399a6a3506fa3eec3e002206f3755071c
Akhmad Qasim <akhmadqasim@gmail.com>
2023-04-02 18:06:14 +0800

    Initial Commit

---
+++ Model/mongodb.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+
+cluster = MongoClient(
+    "mongodb+srv://fauzan:CPbQN3LiePx39fmNPfssAtYkq3Q7jJACCP7dMNft@sandbox.u1rbk.mongodb.net/?retryWrites=true&w=majority")
+
+db = cluster["traveloka"]
+
+dataAcc = db["account"]
+dataFlight = db["flight"]
+dataHotel = db["hotel"]
---
+++ View/user_view.py
@@ -0,0 +1,23 @@
+from Controller import flight_controller as fc
+class MenuUser:
+    def __init__(self):     
+        print("==================================")
+        print("|            M E N U             |")
+        print("==================================")
+        print("|-----> Menu yang tersedia <-----|")
+        print("|                                |")
+        print("|    1. Tampilkan Profil         |")
+        print("|    2. Cari Tiket Pesawat       |")
+        print("|    3. Tampilkan Tiket Pesawat  |")
+        print("|    4. Tambah Saldo             |")
+        print("|                                |")
+        print("==================================")
+        opsi = str(input("Tentukan opsi anda (1/2/3/4/5): "))
+        if opsi == 1:
+            pass
+        if opsi == 2:
+            pass
+        if opsi == 3:
+            pass
+        if opsi == 4:
+            pass
